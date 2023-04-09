e1fc98c89af0f3f52753864c271684276b41a6f1
Rely Arfadillah <shinmendenz@gmail.com>
2023-04-09 20:55:50 +0800

    input-form-logic

--- lib/module/admin/adm_lcd_data_form/controller/adm_lcd_data_form_controller.dart
+++ lib/module/admin/adm_lcd_data_form/controller/adm_lcd_data_form_controller.dart
@@ -1,14 +1,21 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:lcd_loan/state_util.dart';
 import '../view/adm_lcd_data_form_view.dart';
 
-class AdmLcdDataFormController extends State<AdmLcdDataFormView> implements MvcController {
+class AdmLcdDataFormController extends State<AdmLcdDataFormView>
+    implements MvcController {
   static late AdmLcdDataFormController instance;
   late AdmLcdDataFormView view;
 
   @override
   void initState() {
     instance = this;
+    if (isEditMode) {
+      lcdId = widget.item!["lcd_id"];
+      lcdName = widget.item!["lcd_name"];
+    }
     super.initState();
   }
 
@@ -17,4 +24,31 @@ class AdmLcdDataFormController extends State<AdmLcdDataFormView> implements MvcC
 
   @override
   Widget build(BuildContext context) => widget.build(context, this);
+
+  bool get isEditMode {
+    return widget.item != null;
+  }
+
+  String? lcdId;
+  String? lcdName;
+
+  String qrCodeData = 'data';
+
+  doSaveData() async {
+    if (isEditMode) {
+      //await Save Edited Data
+    }
+    Map<String, dynamic> lcdData = {
+      "lcd_id": lcdId,
+      "lcd_name": lcdName,
+    };
+
+    qrCodeData = lcdData.toString();
+    setState(() {});
+    log("Lcd Data = $qrCodeData");
+    //else await add new data
+
+    //show dialog data saved
+    // Get.back();
+  }
 }
--- lib/module/admin/adm_lcd_data_form/view/adm_lcd_data_form_view.dart
+++ lib/module/admin/adm_lcd_data_form/view/adm_lcd_data_form_view.dart
@@ -1,40 +1,55 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+
 import 'package:lcd_loan/core.dart';
 import 'package:lcd_loan/module/admin/adm_lcd_data_form/widget/adm_save_button.dart';
 import 'package:lcd_loan/shared/util/validator/validator.dart';
 import 'package:lcd_loan/shared/widget/appBar_title/app_bar_title.dart';
 import 'package:lcd_loan/shared/widget/input_field/q_text_field.dart';
-import 'package:qr_flutter/qr_flutter.dart';
+
 import '../controller/adm_lcd_data_form_controller.dart';
 
 class AdmLcdDataFormView extends StatefulWidget {
-  const AdmLcdDataFormView({Key? key}) : super(key: key);
+  final Map? item;
+  const AdmLcdDataFormView({
+    Key? key,
+    this.item,
+  }) : super(key: key);
 
   Widget build(context, AdmLcdDataFormController controller) {
     controller.view = this;
 
     return Scaffold(
       appBar: AppBar(
-        title: const AppBarTitle(title: 'Add Lcd Data'),
+        title: controller.isEditMode
+            ? const AppBarTitle(title: 'Edit Data')
+            : const AppBarTitle(title: 'Add Data'),
       ),
       body: Column(
         children: [
+          const SizedBox(
+            height: 20.0,
+          ),
           Center(
             child: QrImage(
-              data: 'data',
+              data: controller.qrCodeData,
               version: QrVersions.auto,
               size: 150,
             ),
           ),
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 15),
+            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
             child: Column(
               children: [
                 QTextField(
                   label: "Nama Proyektor LCD",
                   validator: Validator.required,
                   suffixIcon: Icons.abc_rounded,
-                  onChanged: (value) {},
+                  value: controller.lcdName,
+                  onChanged: (value) {
+                    controller.lcdName = value;
+                  },
                 ),
                 const SizedBox(
                   height: 20,
@@ -43,7 +58,10 @@ class AdmLcdDataFormView extends StatefulWidget {
                   label: "ID Proyektor LCD",
                   validator: Validator.required,
                   suffixIcon: Icons.abc_rounded,
-                  onChanged: (value) {},
+                  value: controller.lcdId,
+                  onChanged: (value) {
+                    controller.lcdId = value;
+                  },
                 ),
               ],
             ),
@@ -51,7 +69,7 @@ class AdmLcdDataFormView extends StatefulWidget {
         ],
       ),
       floatingActionButton: AdmSaveButton(
-        onTap: () {},
+        onTap: () => controller.doSaveData(),
       ),
     );
   }
