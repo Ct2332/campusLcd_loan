a4e5888d52b513e894bbcc99d5e12b54ca697322
Rely Arfadillah <shinmendenz@gmail.com>
2023-04-09 21:37:51 +0800

    firebase-initialize

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
-import 'package:lcd_loan/state_util.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:lcd_loan/core.dart';
 import 'package:flutter/material.dart';
+import 'package:lcd_loan/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
